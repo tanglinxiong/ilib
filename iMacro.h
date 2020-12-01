56b513514eb797729b6576bd33a97411a78b854b
tanglinxiong <1537650346@qq.com>
2020-12-01 09:48:15 +0800

    Add iMacro.h

---
+++ iMacro.h
@@ -0,0 +1,20 @@
+/*********************************************
+Author:Tanglx
+Date:2020/12/01
+FileName:iMacro.h
+一些常用的宏
+**********************************************/
+#ifndef _FILENAME_IMACRO_
+#define _FILENAME_IMACRO_
+
+//表示入参
+#define _i
+//表示出参
+#define _o
+//表示入出参
+#define _io
+
+//装换字符串
+#define TOSTR(x) #x
+
+#endif
